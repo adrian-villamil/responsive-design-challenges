283dbbffa6b49b9c6bcb5035be038ff46d461d3b
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-11-03 00:40:39 -0500

    Replace the shadow and linear gradient classes for a svg image.

--- src/components/meet-team-section/grid-list-item.tsx
+++ src/components/meet-team-section/grid-list-item.tsx
@@ -10,13 +10,9 @@ export const GridListItem = ({ name, role, image, bgColor }: Props) => {
     <div
       style={{
         '--bg-color': bgColor,
-        '--image-url': `url("${image}")`
+        '--image-url': `url('/meet-team-section/Gradient.svg'), url("${image}")`
       } as React.CSSProperties}
-      className={
-        `relative bg-[image:var(--image-url)] bg-[length:100%] hover:bg-[image:var(--image-url)] hover:bg-[length:110%] bg-center bg-no-repeat bg-[color:var(--bg-color)]
-        shadow-[inset_0_-120px_120px_-120px,_inset_0_-90px_90px_-90px,_inset_0_-60px_60px_-60px,_inset_0_-30px_30px_-30px,_inset_0_-10px_10px_-10px,_inset_0_-2px_2px_-2px] 
-        before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-gradient-to-b before:from-transparent before:from-30% before:to-white/15 transition-all`
-      }
+      className="relative bg-[image:var(--image-url)] bg-[length:100%] bg-[position:bottom,_center] bg-no-repeat hover:bg-[image:var(--image-url)] hover:bg-[length:100%,_110%] bg-[color:var(--bg-color)] transition-all"
     >
       <div className="absolute left-0 bottom-0 z-10 w-full pl-6 pb-4">
         <h1 className="text-xl font-medium text-white">{name}</h1>
